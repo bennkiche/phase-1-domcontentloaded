be3defd4f704e3cb937397727eebb1ae8d3284d3
Benn <bennkiche7@gmail.com>
2024-10-05 23:29:00 +0300

    submit task

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Select the paragraph element
+    const paragraph = document.querySelector('p');
+    
+    // Change the initial text
+    paragraph.textContent = "This is really cool!";
+    setTimeout(() => {
+      paragraph.textContent = "This is really cool!";
+    }, 0); 
+  });
+  
